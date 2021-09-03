092e72f5fb6d82193e640ed578a7f3bed5a93879
Bruno Paulino <bruno.paulino@n26.com>
2021-09-03 17:18:48 +0200

    extend common

--- packages/common/src/index.js
+++ packages/common/src/index.js
@@ -2,6 +2,11 @@ function doSomethingCommon() {
   console.log('Doing something common that is different...')
 }
 
+function otherThingFromCommon() {
+  console.log('other thing from common...')
+}
+
 module.exports = {
-  doSomethingCommon
+  doSomethingCommon,
+  otherThingFromCommon
 }
--- packages/servers/src/index.js
+++ packages/servers/src/index.js
@@ -1,9 +1,10 @@
-const { doSomethingCommon } = require('@goldfish/common')
+const { doSomethingCommon, otherThingFromCommon } = require('@goldfish/common')
 
 function startServer() {
   console.log('starting server...')
   doSomethingCommon()
   console.log('Server started with new code...')
+  otherThingFromCommon()
 }
 
 startServer()
