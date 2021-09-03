7a2e25e4a7625eda68c929212249969b25fb293f
Bruno Paulino <bruno.paulino@n26.com>
2021-09-03 17:55:02 +0200

    Do something new

--- packages/servers/src/index.js
+++ packages/servers/src/index.js
@@ -3,8 +3,8 @@ const { doSomethingCommon, otherThingFromCommon } = require('@goldfish/common')
 function startServer() {
   console.log('starting server...')
   doSomethingCommon()
-  console.log('Server started with new code...')
   otherThingFromCommon()
+  console.log('Server started with new code for new release...')
 }
 
 startServer()
