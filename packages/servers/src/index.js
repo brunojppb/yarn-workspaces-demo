adec0b2cf7d4fe3c9ff1a27000b8d066af880a2e
Bruno Paulino <bruno.paulino@n26.com>
2021-09-03 17:16:00 +0200

    Changed server module code

--- packages/servers/src/index.js
+++ packages/servers/src/index.js
@@ -3,7 +3,7 @@ const { doSomethingCommon } = require('@goldfish/common')
 function startServer() {
   console.log('starting server...')
   doSomethingCommon()
-  console.log('Server started...')
+  console.log('Server started with new code...')
 }
 
 startServer()
